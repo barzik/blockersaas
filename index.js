d8cd2e81bbaade895ed3b4c6d4d3b5a375f9cb0c
Ran Bar-Zik <ran@bar-zik.com>
2017-05-25 19:34:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let request = require('request');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, function () {
+  console.log('Dori blocker app listening on port 3000!');
+});
+
+app.get('/isitforbidden', function (req, res) {
+  let URL = req.param('url'); //'http://mizbalax.com/sue-facebook';
+  let forbiddenURL = req.param('badURL') || 'mizbala.com';
+
+  request(URL, function (error, response, body) {
+    console.log('statusCode:', response && response.statusCode); 
+    if (error) {
+      res.status(500).send({ error });
+    }
+
+    let answer = findDori(body, forbiddenURL);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ result : answer }));
+  });
+});
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+function findDori(body, forbiddenURL) {
+  const myRegexp = /<iframe.*src=["|'](.*)["|'] /gm;
+  match = myRegexp.exec(body);
+  if (match && match[1] && match[1].includes(forbiddenURL) ) {
+    console.log(`Got info from ${match[1]}`);
+    return true;
+  } else {
+    return false;
+  }
+}
